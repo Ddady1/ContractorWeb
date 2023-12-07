47d91af908ee45ad1fd0e0fdf2ccc28970f27588
Ddady1 <ddady1@gmail.com>
2023-12-07 15:52:49 +0200

    dsfsd

--- tabs.py
+++ tabs.py
@@ -10,6 +10,7 @@ with ui.column().classes("w-full items-center"):
     with ui.tab_panels(configs, value=a_tab).classes("column items-center justify-center"):
         with ui.tab_panel(a_tab):
             ui.card().style("min-height: 600px").style("min-width: 700px")
+            #ui.button('Next', on_click=ui.tab_panels(configs, value=b_tab).classes("column items-center justify-center"))
         with ui.tab_panel(b_tab):
             ui.card().style("min-height: 500px").style("min-width: 500px")
         with ui.tab_panel(c_tab):
