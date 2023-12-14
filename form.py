dbac4c0fc662289946bdce8783cd1ff8ff311ab5
Ddady1 <ddady1@gmail.com>
2023-12-14 15:44:36 +0200

    form start 1

--- form.py
+++ form.py
@@ -4,23 +4,23 @@ from tkinter import ttk
 
 root = tk.Tk()
 root.title('Form')
-root.geometry('800x600+350+150')
+root.geometry('760x600+350+150')
 
 root.rowconfigure(0, weight=2)
 name_lb = tk.Label(root, text='Product:')
-name_en = tk.Entry(root)
+name_en = tk.Entry(root, width=35)
 manuf_lb = tk.Label(root, text='Manufacturer:')
-manuf_en = tk.Entry(root, width=50)
+manuf_en = tk.Entry(root, width=40)
 qty_lb = tk.Label(root, text='Quantity:')
-qty_en = tk.Entry(root)
+qty_en = tk.Entry(root, width=10)
 
 
-name_lb.grid(column=0, row=0, padx=5, pady=5, sticky=tk.N)
-name_en.grid(column=1, row=0, padx=5, pady=5)
+name_lb.grid(column=0, row=0, padx=5, pady=5)
+name_en.grid(column=1, row=0)
 manuf_lb.grid(column=2, row=0, padx=5, pady=5)
-manuf_en.grid(column=3, row=0, padx=5, pady=5)
+manuf_en.grid(column=3, row=0)
 qty_lb.grid(column=4, row=0, padx=5, pady=5)
-qty_en.grid(column=5, row=0, padx=5, pady=5)
+qty_en.grid(column=5, row=0)
 
 
 root.mainloop()
