5e0271482e54c9744c4a208ee981fad35ed89f5e
Dady <ddady1@gmail.com>
2024-01-02 23:41:15 +0200

    7854

--- Setup_frames.py
+++ Setup_frames.py
@@ -10,8 +10,10 @@ def main_dis():
     main_frame.destroy()
 
 
-def setup_next():
+def setup_next(frames_list):
     global count
+
+
 
 
 
@@ -99,7 +101,7 @@ bottom_frame = ttkb.Frame(root)
 cancel_btn = ttkb.Button(bottom_frame, text='Cancel', width=8, command=main_dis)
 cancel_btn.pack(side=ttkb.LEFT, padx=20, pady=5)
 
-next_btn = ttkb.Button(bottom_frame, text='Next', width=8, command=setup_next)
+next_btn = ttkb.Button(bottom_frame, text='Next', width=8, command=lambda: setup_next(frames))
 next_btn.pack(side=ttkb.RIGHT, padx=20, pady=5)
 
 prev_btn = ttkb.Button(bottom_frame, text='Previous', width=8, command=setup_prev())
