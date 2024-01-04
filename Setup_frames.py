59620d53151f485e8fef816bf96cf6952232c0a1
Ddady1 <ddady1@gmail.com>
2024-01-04 09:33:29 +0200

    original way according to youtube

--- Setup_frames.py
+++ Setup_frames.py
@@ -84,31 +84,47 @@ root.title('Contractor Wizard')
 root.geometry('630x480+450+250')
 root.resizable(False, False)
 
+# Main Frame contaiuner
+
 main_frame = ttkb.Frame(root)
 
-setup_info_lf = tkinter.ttk.Labelframe
-db_select_lf = tkinter.ttk.Labelframe
-sqlite_set_lf = tkinter.ttk.Labelframe
-frames = [setup_info_lf, db_select_lf, sqlite_set_lf]
-count = 0
+# Frame 1
 
-
-
-'''setup_info_lf = ttkb.LabelFrame(main_frame, text='About', bootstyle='primary')
-setup_top_lbl = ttkb.Label(main_frame, text='  Welcome to Contractor setup', bootstyle='primary inverse', font=('Helvetica', 20))
+setup_info_lf = ttkb.LabelFrame(main_frame, text='About', bootstyle='primary')
+setup_top_lbl = ttkb.Label(main_frame, text='  Welcome to Contractor setup', bootstyle='primary inverse',
+                               font=('Helvetica', 20))
 setup_top_lbl.pack(side=TOP, fill=X, padx=2, pady=1, ipady=10)
 setup_info_lbl = ttkb.Label(setup_info_lf, text='Contractor app was design to help you keep track of all your\n'
-                                           'contracts and to manage that in one place.\n\n'
-                                           'Contractor also will remind you when your contracts are about to\n'
-                                           'be expired.\n\n'
-                                           'Contractor is a Python base application and can work with various\n'
-                                           'of databases such ad SQLite, MySQL, MongoDB and more...\n',
-                      font=('Helvetica', 15), bootstyle='primary')
+                                                    'contracts and to manage that in one place.\n\n'
+                                                    'Contractor also will remind you when your contracts are about to\n'
+                                                    'be expired.\n\n'
+                                                    'Contractor is a Python base application and can work with various\n'
+                                                    'of databases such ad SQLite, MySQL, MongoDB and more...\n',
+                                font=('Helvetica', 15), bootstyle='primary')
 setup_info_lbl.pack()
-setup_info_lf.pack(pady=50)'''
+setup_info_lf.pack(pady=50)
+
+# Frame 2
+
+db_select_lf = ttkb.LabelFrame(main_frame, text='Select Database', bootstyle='primary')
+db_select_top_lbl = ttkb.Label(main_frame, text='  Choose your preferred Database', bootstyle='primary inverse',
+                               font=('Helvetica', 20))
+db_select_top_lbl.pack(side=TOP, fill=X, padx=2, pady=1, ipady=10)
+db_list = ['SQLite', 'MySQL', 'MongoDB', 'Excel File']
+db_selection_var = StringVar()
+i = 200
+for db in db_list:
+    ttkb.Radiobutton(db_select_lf, bootstyle='primary', variable=db_selection_var,
+                        text=db, value=db).pack(pady=20, padx=20, fill=BOTH)
+
+# Frame 3
+
+sqlite_set_lf = ttkb.Label(main_frame, text='Creating SQlite database', bootstyle='primary')
 
 main_frame.pack(pady=1, fill=BOTH, expand=True)
 
+
+frames = [setup_info_lf, db_select_lf, sqlite_set_lf]
 
 
 bottom_frame = ttkb.Frame(root)
@@ -123,13 +139,7 @@ prev_btn = ttkb.Button(bottom_frame, text='Previous', width=8, command=setup_nex
 prev_btn.pack(side=ttkb.RIGHT, padx=10, pady=5)
 
 
-
 bottom_frame.pack(side=ttkb.BOTTOM, pady=10, fill=X)
-
-
-
-#info_frame()
-
 
 
 root.mainloop()
