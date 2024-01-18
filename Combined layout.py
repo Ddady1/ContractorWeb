ceb21f7eda72d34d711678f9e36a764b6634caf2
Dady <ddady1@gmail.com>
2024-01-18 21:31:01 +0200

    class

--- Combined layout.py
+++ Combined layout.py
@@ -1,17 +1,18 @@
 import tkinter as tk
-from tkinter import ttk
+import ttkbootstrap as ttk
+#from tkinter import ttk
 
 
 # window
-window = tk.Tk()
+window = ttk.Window(themename='sandstone')
 window.title('Combined layout')
 window.geometry('600x600')
 window.minsize(600, 600)
 
 
 # main layout widgets
-menu_frame = tk.Frame(window)
-main_frame = tk.Frame(window)
+menu_frame = ttk.Frame(window)
+main_frame = ttk.Frame(window)
 
 # main place layout
 menu_frame.place(x=0, y=0, relwidth=0.3, relheight=1)
@@ -34,9 +35,9 @@ entry = ttk.Entry(menu_frame)
 menu_frame.columnconfigure((0, 1, 2), weight=1, uniform='a')
 menu_frame.rowconfigure((0, 1, 2, 3, 4), weight=1, uniform='a')
 
-menu_button1.grid(row=0, column=0, sticky='nsew', columnspan=2)
-menu_button2.grid(row=0, column=2, sticky='nsew')
-menu_button3.grid(row=1, column=0, columnspan=3, sticky='nsew')
+menu_button1.grid(row=0, column=0, sticky='nsew', columnspan=2, padx=4, pady=4)
+menu_button2.grid(row=0, column=2, sticky='nsew', padx=4, pady=4)
+menu_button3.grid(row=1, column=0, columnspan=3, sticky='nsew', padx=4, pady=4)
 
 menu_slider1.grid(row=2, column=0, rowspan=2, sticky='nsew', pady=20)
 menu_slider2.grid(row=2, column=2, rowspan=2, sticky='nsew', pady=20)
@@ -50,11 +51,11 @@ menu_toggle2.pack(side='left', expand=True)
 entry.place(relx=0.5, rely=0.95, relwidth=0.9, anchor='center')
 
 # main widgets
-entry_frame1 = ttk.Entry(main_frame)
+entry_frame1 = ttk.Frame(main_frame)
 main_label1 = ttk.Label(entry_frame1, text='Label 1', background='red')
 main_button1 = ttk.Button(entry_frame1, text='Button 1')
 
-entry_frame2 = ttk.Entry(main_frame)
+entry_frame2 = ttk.Frame(main_frame)
 main_label2 = ttk.Label(entry_frame2, text='Label 2', background='blue')
 main_button2 = ttk.Button(entry_frame2, text='Button 2')
 
@@ -64,6 +65,9 @@ entry_frame2.pack(side='left', expand=True, fill='both', padx=20, pady=20)
 
 main_label1.pack(expand=True, fill='both')
 main_button1.pack(expand=True, fill='both', pady=10)
+
+main_label2.pack(expand=True, fill='both')
+main_button2.pack(expand=True, fill='both', pady=10)
 #ttk.Label(entry_frame1, background='red').pack(expand=True, fill='both')
 #ttk.Label(entry_frame2, background='yellow').pack(expand=True, fill='both')
 # run
