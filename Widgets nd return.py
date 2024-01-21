3d00b47541f32b71946793a98d5a083b8b622447
Ddady1 <ddady1@gmail.com>
2024-01-21 14:05:50 +0200

    widget and return end

--- Widgets nd return.py
+++ Widgets nd return.py
@@ -4,7 +4,7 @@ import ttkbootstrap as ttk
 
 
 class Segment(ttk.Frame):
-    def __init__(self, parent, label_text, button_text):
+    def __init__(self, parent, label_text, button_text, exercise_text):
         super().__init__(master=parent)
 
         # grid layout
@@ -12,8 +12,15 @@ class Segment(ttk.Frame):
         self.columnconfigure((0, 1, 2), weight=1, uniform='a')
         ttk.Label(self, text=label_text).grid(row=0, column=0, sticky='nsew')
         ttk.Button(self, text=button_text).grid(row=0, column=1, sticky='nsew')
-
+        self.create_exercise_box(exercise_text).grid(row=0, column=2, sticky='nsew')
         self.pack(expand=True, fill='both', padx=10, pady=10)
+
+    def create_exercise_box(self, text):
+        frame = ttk.Frame(master=self)
+        ttk.Entry(frame).pack(expand=True, fill='both', padx=5, pady=5)
+        ttk.Button(frame, text=text).pack(expand=True, fill='both', padx=5)
+
+        return frame
 
 # window
 window = ttk.Window(themename='sandstone')
@@ -21,11 +28,11 @@ window.title('Widgets and return')
 window.geometry('400x600')
 
 # widgets
-Segment(window, 'label', 'button')
-Segment(window, 'test', 'click')
-Segment(window, 'hello', 'test')
-Segment(window, 'bye', 'launch')
-Segment(window, 'last one', 'exit')
+Segment(window, 'label', 'button', 'test')
+Segment(window, 'test', 'click', 'something')
+Segment(window, 'hello', 'test', 'other')
+Segment(window, 'bye', 'launch', 'exit')
+Segment(window, 'last one', 'exit', 'end')
 
 
 # run
