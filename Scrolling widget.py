076e4ecd149c0b166fd1663f7b1f04b2f92e321d
Ddady1 <ddady1@gmail.com>
2024-01-31 15:49:58 +0200

    scroll widget end

--- Scrolling widget.py
+++ Scrolling widget.py
@@ -14,26 +14,48 @@ class ListFrame(ttk.Frame):
         self.list_height = self.item_number * item_height
 
         # canvas
-        self.canvas = tk.Canvas(self, background='red', scrollregion=(0, 0, 500, self.list_height))
+        self.canvas = tk.Canvas(self, background='red', scrollregion=(0, 0, self.winfo_width(), self.list_height))
         self.canvas.pack(expand=True, fill='both')
 
         # display frame
         self.frame = ttk.Frame(self)
 
         for index, item in enumerate(self.text_data):
-            self.create_item(index, item)
+            self.create_item(index, item).pack(expand=True, fill='both', pady=4, padx=10)
 
-        self.canvas.create_window((0, 0), window=self.frame, anchor='nw', width=500, height=self.list_height)
+        #self.canvas.create_window((0, 0), window=self.frame, anchor='nw', width=self.winfo_width(), height=self.list_height)
+
+        self.scrollbary = ttk.Scrollbar(self, orient='vertical', command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.scrollbary.set)
+        self.scrollbary.place(relx=1, rely=0, relheight=1, anchor='ne')
+
+        # events
+        self.canvas.bind_all('<MouseWheel>', lambda event: self.canvas.yview_scroll(-int(event.delta / 60), 'units'))
+        self.bind('<Configure>', self.update_size)
+
+    def update_size(self, event):
+        if self.list_height >= self.winfo_height():
+            height = self.list_height
+            self.canvas.bind_all('<MouseWheel>', lambda event: self.canvas.yview_scroll(-int(event.delta / 60), 'units'))
+            self.scrollbary.place(relx=1, rely=0, relheight=1, anchor='ne')
+        else:
+            height = self.winfo_height()
+            self.canvas.unbind_all('<MouseWheel>')
+            self.scrollbary.place_forget()
+        self.canvas.create_window((0, 0), window=self.frame, anchor='nw', width=self.winfo_width(), height=height)
 
     def create_item(self, index, item):
         frame = ttk.Frame(self.frame)
 
         # grid layout
-        frame.rowconfigure(0, window=1)
+        frame.rowconfigure(0, weight=1)
         frame.columnconfigure((0, 1, 2, 3, 4), weight=1, uniform='a')
 
         # widgets
-        ttk.Label(frame, text=f'#{index}')
+        ttk.Label(frame, text=f'#{index}').grid(row=0, column=0)
+        ttk.Label(frame, text=f'#{item[0]}').grid(row=0, column=1)
+        ttk.Button(frame, text=f'{item[1]}').grid(row=0, column=2, columnspan=3, sticky='nsew')
+        return frame
 
 # setup
 
