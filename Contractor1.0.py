a17ea3a4f7665b299d8c7ea6adddb0df9e797827
dady <ddady1@gmail.com>
2024-02-26 15:24:28 +0200

    data frame

--- Contractor1.0.py
+++ Contractor1.0.py
@@ -1,7 +1,31 @@
 import tkinter as tk
 import ttkbootstrap as ttk
+from ttkbootstrap.tableview import Tableview
+from ttkbootstrap.constants import *
 
+def read_data():
+    coldata = [
+        {"text": "LicenseNumber", "stretch": False},
+        "CompanyName",
+        {"text": "UserCount", "stretch": False},
+    ]
 
+    rowdata = [
+        ('A123', 'IzzyCo', 12),
+        ('A136', 'Kimdee Inc.', 45),
+        ('A158', 'Farmadding Co.', 36)
+    ]
+
+    dt = Tableview(
+        master=data_frame,
+        coldata=coldata,
+        rowdata=rowdata,
+        paginated=True,
+        searchable=True,
+        bootstyle=PRIMARY,
+        stripecolor=(None, None),
+    )
+    dt.pack(fill=BOTH, expand=YES, padx=10, pady=10)
 # Window
 window = ttk.Window(themename='sandstone')
 window.title('Contractor 1.0')
@@ -23,7 +47,7 @@ item_frame.place(relx=0.5, rely=0.05, relwidth=0.5, relheight=0.95)
 
 # Lebels for checkups
 menu_label = ttk.Label(menu_frame, text='MENU', background='blue').pack(side='top', expand=True, fill='both')
-data_label = ttk.Label(data_frame, text='DATA', background='red').pack(side='left', expand=True, fill='both')
+#data_label = ttk.Label(data_frame, text='DATA', background='red').pack(side='left', expand=True, fill='both')
 item_label = ttk.Label(item_frame, text='ITEM', background='green').pack(side='left', expand=True, fill='both')
 
 # Menubar
@@ -35,12 +59,12 @@ window.config(menu=menu_bar)
 
 file_menu = ttk.Menu(menu_bar, tearoff=False)
 menu_bar.add_cascade(label='File', menu=file_menu)
-file_menu.add_command(label='New...')
+file_menu.add_command(label='Open...')
 file_menu.add_separator()
 file_menu.add_command(label='Print')
 file_menu.add_separator()
 
-# Menubar export submenu
+# Menubar File->export submenu
 
 export_submenu = ttk.Menu(file_menu, tearoff=False)
 export_submenu.add_command(label='to CSV')
@@ -49,7 +73,7 @@ export_submenu.add_command(label='to PDF')
 file_menu.add_cascade(label='Export...', menu=export_submenu)
 
 
-# Menubar import submenu
+# Menubar File->import submenu
 import_submenu = ttk.Menu(file_menu, tearoff=False)
 import_submenu.add_command(label='from CSV')
 import_submenu.add_command(label='from Excel')
@@ -57,10 +81,22 @@ import_submenu.add_command(label='from Excel')
 file_menu.add_cascade(label='Import...', menu=import_submenu)
 
 
-# Menubar exit
+# Menubar File->exit
 
 file_menu.add_separator()
-file_menu.add_command(label='Exit')
+file_menu.add_command(label='Exit', command=window.quit)
 
 
-window.mainloop()
+# Menubar Edit
+
+edit_menu = ttk.Menu(menu_bar, tearoff=False)
+menu_bar.add_cascade(label='Edit', menu=edit_menu)
+edit_menu.add_command(label='Add Contract')
+edit_menu.add_command(label='Edit Contract')
+edit_menu.add_separator()
+edit_menu.add_command(label='Select All')
+
+
+if __name__ == "__main__":
+    read_data()
+    window.mainloop()
