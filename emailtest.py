fa680e4807cdb2dac3e9ee06950b3a839bb32a50
dady <ddady1@gmail.com>
2024-03-10 16:30:11 +0200

    finished creating column list

---
+++ emailtest.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+server_account = 'dady@tadiran-group.co.il' #authorized account
+server_password = 'DDrrdd150570' #password for account
+
+msg = MIMEMultipart()
+msg["Subject"] = "Test from Python" # subject of message
+msg["From"] = "dady@tadiran-group.co.il" #valid user on server
+msg["To"] = "ddady1@gmail.com" #list of recipients if more than one
+msg["Cc"] = "" #optional
+body = MIMEText("Python ROCKS") #body of message
+msg.attach(body)
+
+smtp = smtplib.SMTP('smtp.office365.com',587)
+smtp.ehlo()
+smtp.starttls()
+smtp.login(server_account, server_password)
+smtp.sendmail(msg["From"], msg["To"].split(",") + msg["Cc"].split(","), msg.as_string())
+smtp.quit()
