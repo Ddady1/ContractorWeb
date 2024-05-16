33b8b10cabaa43a252fff461104807b0822f3a6a
Ddady1 <ddady1@gmail.com>
2024-05-16 15:37:26 +0300

    edit item

--- emailtest.py
+++ emailtest.py
@@ -2,13 +2,13 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-server_account = 'dady@tadiran-group.co.il' #authorized account
-server_password = 'DDrrdd150570' #password for account
+server_account = 'XXXX@tadiran-group.co.il' #authorized account
+server_password = 'XXXXXXXXXXXXXXX' #password for account
 
 msg = MIMEMultipart()
 msg["Subject"] = "Test from Python" # subject of message
-msg["From"] = "dady@tadiran-group.co.il" #valid user on server
-msg["To"] = "ddady1@gmail.com" #list of recipients if more than one
+msg["From"] = "XXXX@tadiran-group.co.il" #valid user on server
+msg["To"] = "XXXXX@gmail.com" #list of recipients if more than one
 msg["Cc"] = "" #optional
 body = MIMEText("Python ROCKS") #body of message
 msg.attach(body)
